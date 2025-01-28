3871065e61a4c5d839085937bad4c29e50d083c6
recourcefulcoder <grigorij.kirichenko1@gmail.com>
2025-01-28 13:59:00 +0100

    API added, fixture and translations updated

--- dionysus/api/urls.py
+++ dionysus/api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from utils.views import do_nothing
+from rest_framework.urlpatterns import format_suffix_patterns
 
-app_name = "api"
-urlpatterns = [path("delete/<int:pk>/", view=do_nothing, name="delete")]
+from .views import OrderDetail, OrderList
+
+app_name = 'api'
+urlpatterns = [
+    path('', view=OrderList.as_view(), name='list'),
+    path('<int:pk>/', view=OrderDetail.as_view(), name='delete'),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ dionysus/api/views.py
@@ -0,0 +1,15 @@
+from orders.models import Order
+
+from rest_framework import generics
+
+from .serializers import OrderSerializer
+
+
+class OrderList(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
